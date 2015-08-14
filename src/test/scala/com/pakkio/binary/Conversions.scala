6217495ded2f266ddd78c7c5c8c357911ce40e89
Salahzar Stenvaag <salahzar@gmail.com>
2015-08-14 20:08:47 +0200

    implementing some R functions

--- src/test/scala/com/pakkio/binary/Conversions.scala
+++ src/test/scala/com/pakkio/binary/Conversions.scala
@@ -3,15 +3,15 @@ package com.pakkio.binary
 import org.scalatest.FunSuite
 import Implicits._
 
-class Conversions extends FunSuite  {
+class Conversions extends FunSuite {
 
   test("testConvertStringsToByteArrays") {
-      val s="alle falde"
+    val s = "alle falde"
 
-      val ba:Array[Byte]=s
+    val ba: Array[Byte] = s
 
-      assert(ba(0)===97)
-      assert(ba.length===s.length)
+    assert(ba(0) === 97)
+    assert(ba.length === s.length)
 
   }
 
