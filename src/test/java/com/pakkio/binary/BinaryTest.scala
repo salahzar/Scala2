08dd3271d405d9196a3044c80ac4f46ee5519bf2
Salahzar Stenvaag <salahzar@gmail.com>
2015-08-29 14:19:30 +0200

    set Binary as new class for Binary Buffers and some cryptaes modifications

---
+++ src/test/java/com/pakkio/binary/BinaryTest.scala
@@ -0,0 +1,62 @@
+package com.pakkio.binary
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by Francesco on 29/08/2015.
+ */
+class BinaryTest extends FunSuite {
+
+
+  test("testFillTo") {
+    val b0=new Binary(Array[Byte]())
+    assert(b0.size===0)
+    val b=new Binary(Array[Byte](1,1,1),4)
+    assert(b.size===4)
+    val b2=new Binary(Array[Byte](5,5,5,5,5),4)
+    assert(b2.size===4)
+
+  }
+  test("testUnderlying") {
+    val b0=new Binary(Array[Byte]())
+    assert(b0.underlying===0)
+    val b=new Binary(Array[Byte](1,1,1),4)
+    assert(b.underlying===256*256+256+1)
+    val b2=new Binary(Array[Byte](5,5,5,5,5),4)
+    assert(b2.underlying===(5 * Math.pow(256,3) + 5 * Math.pow(256,2) + 5*256 + 5*1))
+
+
+  }
+
+
+  test("testSize") {
+
+    val bytes: Array[Byte] = (0 until 50).map(i => i.toByte).toArray
+    val b=new Binary(bytes)
+    assert(b.size===50)
+
+  }
+
+
+  test("testToIntArray") {
+    val b=new Binary(Array[Byte](1,2,-3),4)
+    assert(b.toIntArray === Array[Int](0,1,2,253))
+  }
+
+  test("testToByteArray") {
+    val b=Binary(Array[Byte](1,2,-3),4)
+    assert(b.toByteArray === Array[Byte](0,1,2,-3))
+  }
+  test("from hex"){
+    val b=Binary.fromHex("ffff",4)
+    assert(b.toByteArray===Array[Byte](0,0,-1,-1))
+  }
+  test("to hex"){
+    val b=Binary.fromHex("ffff",4).toHex()
+    assert(b==="0000ffff")
+  }
+  test("xor tests"){
+    pending
+  }
+
+}
--- src/test/scala/com/pakkio/binary/CryptAES-CBC.scala
+++ src/test/scala/com/pakkio/binary/CryptAES-CBC.scala
@@ -1,14 +1,18 @@
 
 import java.nio.charset.{StandardCharsets, Charset}
 
-import com.pakkio.binary.Buffer
+import com.pakkio.binary.{Binary, Buffer}
 import com.pakkio.binary.Week2Crypto.R
+
 import org.scalatest.FunSuite
 import sun.misc.HexDumpEncoder
 import xyz.wiedenhoeft.scalacrypt.blockciphers.AES128
 import xyz.wiedenhoeft.scalacrypt._
 import xyz.wiedenhoeft.scalacrypt.modes.CBC
 import xyz.wiedenhoeft.scalacrypt.paddings.PKCS7Padding
+
+import scala.collection.mutable
+import scala.util.{Try, Failure, Success}
 
 
 class CBCTest extends FunSuite {
@@ -27,26 +31,47 @@ class CBCTest extends FunSuite {
     new BlockCipherSuite(aes, cbc, pkcs7)
 
   }
+  def cryptCBC(key:Key, iv:Array[Byte], msg: String): Try[Array[Byte]] ={
 
-  implicit def string2byte(s:String):List[Byte]=s.getBytes(StandardCharsets.US_ASCII).toList
-  implicit def seq2list(s:Seq[Byte]):List[Byte]=s.toList
+    val ivBlock = new Binary(iv,16)
+    cryptCipher(key,ivBlock.toByteArray).encrypt(msg.map(_.toByte)) match {
+      case Success(x) => Success(ivBlock.toByteArray ++ x)
+      case Failure(e) => {
+        print(s"Failing cryptCBC because  error $e")
+        Failure(e)
+      }
+    }
+  }
+  def decryptCBC(key:Key, cipherText:Array[Byte]): Try[String] ={
+    val iv=cipherText.slice(0,16)
+    val rest=cipherText.drop(16)
+    cryptCipher(key,iv).decrypt(rest) match {
+      case Success(s) => Success(s.map(byte => byte.toChar).mkString)
+      case Failure(e) => {
+        print(s"Failing decryptCBC because error $e")
+        Failure(e)
+      }
+    }
 
-  val key: Key = Key.generate
+  }
+
+  val key: Key = Key.generate[SymmetricKey128]
 
   test("testing encode and decode") {
-
-    val suite=cryptCipher(key,iv)
     val original="Initial Value"
-    val encrypted = suite.encrypt(original).get
-
-    val buffer = Buffer.fromBytes(params('iv))Buffer.fromBytes(encrypted)
-    println(s"encrypted is ${buffer.toHex}")
-
-
-    val decrypted = suite.decrypt(encrypted).get
-
-    val returned = decrypted.map(_.toChar).mkString
-    assert(original==returned)
+    val iv=Array[Byte](1)
+    cryptCBC(key,iv,original).map { encrypted =>
+      //val hex=BaseN(encrypted).toString
+      //println(s"encrypted is ${hex}")
+      decryptCBC(key,encrypted).map { decrypted =>
+        val ascii = decrypted.mkString
+        println(s"decrypted is ${ascii}")
+        assert(original == ascii)
+      }
+    }
+  }
+  test("try to forge a cbc encryption"){
+    val plainText="$100 to Bob"
 
   }
 }
